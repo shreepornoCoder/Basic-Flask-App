1c2c9a7670e1d9a4a81cebf858ee567510112ada
shreepornoCoder <sontos1979@gmail.com>
2024-12-26 21:33:09 +0600

    File Added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, redirect, flash, jsonify
 from config import Config
-from models import db, Student
+from models import db, Student, Teacher
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,23 +12,30 @@ with app.app_context():
 @app.route('/', methods=['GET'])
 def get():
     students = Student.query.all()
-    # TODO find proper response
+    teachers = Teacher.query.all()
+    
     return jsonify([{
         'id': student.id,
         'name': student.name,
         'email': student.email,
         'class_name': student.class_name,
         'admitted_at': student.admitted_at,
-    } for student in students])
+    } for student in students], 
+    [{
+        'id':teacher.id,
+        'name':teacher.name,
+        'subject':teacher.subject,
+        'Joining_Date': teacher.join
+    }for teacher in teachers]
+    )
 
+@app.route('/create/student', methods=['POST'])
+def create_student():
+    stu_name = request.form['name']
+    stu_email = request.form['email']
+    stu_class_name = request.form['class_name']
 
-@app.route('/create', methods=['POST'])
-def create():
-    name = request.form['name']
-    email = request.form['email']
-    class_name = request.form['class_name']
-
-    student = Student(name=name, email=email, class_name=class_name)
+    student = Student(name=stu_name, email=stu_email, class_name=stu_class_name)
     db.session.add(student)
 
     try:
@@ -40,6 +47,22 @@ def create():
     finally:
         return 'Create API called'
 
+@app.route('/create/teacher', methods=['POST'])
+def create_teacher():
+    teacher_name = request.form['name']
+    teacher_subject = request.form['subject']
+
+    teacher = Teacher(name=teacher_name, subject=teacher_subject)
+    db.session.add(teacher)
+
+    try:
+        db.session.commit()
+        flash('Teacher created successfully!')
+    except:
+        db.session.rollback()
+        flash('Error: Teacher could not be created!')
+    finally:
+        return 'Create API called'
 
 @app.route('/edit/<int:id>', methods=['PUT'])
 def edit(id):
@@ -57,8 +80,8 @@ def edit(id):
     finally:
         return 'Update API called'
 
-@app.route('/delete/<int:id>', methods=['DELETE'])
-def delete(id):
+@app.route('/delete/student/<int:id>', methods=['DELETE'])
+def delete_student(id):
     student = Student.query.get_or_404(id)
     
     try:
@@ -71,6 +94,19 @@ def delete(id):
     finally:
         return 'Delete API called'
 
+@app.route('/delete/teacher/<int:id>', methods=['DELETE'])
+def delete_teacher(id):
+    teacher = Teacher.query.get_or_404(id)
+    
+    try:
+        db.session.delete(teacher)
+        db.session.commit()
+        flash('Teacher deleted successfully!')
+    except:
+        db.session.rollback()
+        flash('Error: Teacher could not be deleted!')
+    finally:
+        return 'Delete API called'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -9,3 +9,9 @@ class Student(db.Model):
     email = db.Column(db.String(60), unique=True)
     class_name = db.Column(db.String(60))
     admitted_at = db.Column(db.DateTime, default=datetime.now())
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False, unique=True)
+    subject = db.Column(db.String)
+    join = db.Column(db.DateTime, default = datetime.now())
