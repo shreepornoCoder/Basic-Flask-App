2d83a52643ffbf3999d8df076d640ac531f3149e
shreepornoCoder <sontos1979@gmail.com>
2024-12-26 20:21:14 +0600

    File Added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, flash, redirect, jsonify
+from flask import Flask, request, redirect, flash, jsonify
 from config import Config
 from models import db, Student
 
@@ -9,63 +9,68 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-@app.route('/', methods=["GET"])
+@app.route('/', methods=['GET'])
 def get():
     students = Student.query.all()
-
+    # TODO find proper response
     return jsonify([{
-        'id':student.id,
-        'name':student.name,
-        'email':student.email,
-        'class_name':student.class_name,
-        "admitted_at":student.admitted_at
+        'id': student.id,
+        'name': student.name,
+        'email': student.email,
+        'class_name': student.class_name,
+        'admitted_at': student.admitted_at,
     } for student in students])
+
 
 @app.route('/create', methods=['POST'])
 def create():
-    name = request.form["name"]
+    name = request.form['name']
     email = request.form['email']
-    class_name = request.form["Class"]
+    class_name = request.form['class_name']
 
-    student = Student(name = name, email = email, class_name = class_name)
+    student = Student(name=name, email=email, class_name=class_name)
     db.session.add(student)
 
     try:
         db.session.commit()
-        flash("Student Created Successfully!")
-        return student
+        flash('Student created successfully!')
     except:
         db.session.rollback()
-        flash("Student Couldn't be Created!")
+        flash('Error: Student could not be created!')
+    finally:
+        return 'Create API called'
 
 
 @app.route('/edit/<int:id>', methods=['PUT'])
 def edit(id):
     student = Student.query.get_or_404(id)
-
-    student.email = request.email["email"]
-    student.class_name = request.email["class_name"]
-
+    
+    student.email = request.form['email']
+    student.class_name = request.form['class_name']
+    
     try:
         db.session.commit()
-        flash("Student Updated Successfully!")
-        return student
+        flash('Student updated successfully!')
     except:
         db.session.rollback()
-        flash("Student Couldn't be updated!")
+        flash('Error: Student could not be updated!')
+    finally:
+        return 'Update API called'
 
 @app.route('/delete/<int:id>', methods=['DELETE'])
 def delete(id):
     student = Student.query.get_or_404(id)
-
+    
     try:
         db.session.delete(student)
         db.session.commit()
-        flash("Student Updated Successfully!")
-        return student
+        flash('Student deleted successfully!')
     except:
         db.session.rollback()
-        flash("Student Couldn't be updated!")
+        flash('Error: Student could not be deleted!')
+    finally:
+        return 'Delete API called'
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from datetime import datetime
 db = SQLAlchemy()
 
 class Student(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(100), nullable = False) # name will be within 100 char
-    email = db.Column(db.String(60), unique = True) 
-    class_name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(60), unique=True)
+    class_name = db.Column(db.String(60))
     admitted_at = db.Column(db.DateTime, default=datetime.now())
